7cb2e515195f08719b857fa30972eb27f2cc492e
Padzitto <mokrienkousa@gmail.com>
2024-12-05 11:13:44 +1000

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+# динамическая типизация
+name = "Pavel" # name-переменная, Pavel-объект
+print(name)
+age = 43
+print(age)
+new_age = age + 3
+print(new_age)
+is_student = True
+print(is_student)
+print("Name:",name,"Age:", age,"New age:",new_age, "Is student:", is_student)
